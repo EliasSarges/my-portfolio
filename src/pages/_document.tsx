66205c8c4815b31fafd0686641536ad2126202c2
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2023-05-01 12:45:16 -0300

    chore: add metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,42 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Play&display=swap"
           rel="stylesheet"
         />
+
+        <meta name="title" content="Elias Castro" />
+        <meta
+          name="description"
+          content="Desenvolvedor Front End com habilidades em design, criando experiências de usuário excepcionais."
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://my-portfolio-nu-dun.vercel.app/"
+        />
+        <meta property="og:title" content="Elias Castro" />
+        <meta
+          property="og:description"
+          content="Desenvolvedor Front End com habilidades em design, criando experiências de usuário excepcionais."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/38994457?s=400&u=fc450f34e4de342ab76a90b281512edbc7549231&v=4"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://my-portfolio-nu-dun.vercel.app/"
+        />
+        <meta property="twitter:title" content="Elias Castro" />
+        <meta
+          property="twitter:description"
+          content="Desenvolvedor Front End com habilidades em design, criando experiências de usuário excepcionais."
+        />
+        <meta
+          property="twitter:image"
+          content="https://avatars.githubusercontent.com/u/38994457?s=400&u=fc450f34e4de342ab76a90b281512edbc7549231&v=4"
+        />
       </Head>
 
       <body>
